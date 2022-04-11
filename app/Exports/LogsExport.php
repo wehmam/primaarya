541852b0a569e4705d005e0b21d46df4954c21ad
wehmam <readytosurff@gmail.com>
2022-04-11 11:23:05 +0700

    update logs activity2

--- app/Exports/LogsExport.php
+++ app/Exports/LogsExport.php
@@ -10,11 +10,11 @@ class LogsExport implements FromCollection, WithHeadings
 {
   
     public function headings() : array {
-        return ["Date", "Title", "Type", "Email", "Activity", "Ip_Address", "Location", "Url"];
+        return ["Date", "Title", "Type", "Email", "Activity", "Ip_Address", "Location", "Custom_Logs"];
     }
     public function collection()
     {
-        return ActivityLogs::select("created_at", "title", "type", "email", "activity", "ip_address", "location", "url")
+        return ActivityLogs::select("created_at", "title", "type", "email", "activity", "ip_address", "location", "custom_logs")
             ->orderByDesc("id")
             ->get();
     }
